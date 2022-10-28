92b39d872a7250966ef36c10f55f5181dab0c489
Your Name <originating_cagliostro@aleeas.com>
2022-10-28 16:27:01 -0500

    get random number to work

--- 0004/src/main.rs
+++ 0004/src/main.rs
@@ -1,3 +1,11 @@
+extern crate rand;
+use rand::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+
+    for _x in 0..5{
+        let num: u8 = rng.gen_range(0..10);
+        println!("Random number between 0 and 9: {}", num);
+    }
 }
