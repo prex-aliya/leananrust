97fc70d4c6af8cc043a5de8d92e21c79fd0dcf3e
Your Name <originating_cagliostro@aleeas.com>
2022-10-29 00:05:41 -0500

    first working

--- 0004/src/main.rs
+++ 0004/src/main.rs
@@ -1,8 +1,28 @@
+/* sources -
+ * https://docs.rs/fastrand/latest/fastrand/
+ *
+ */
 use fastrand;
+use std::env;
+
 
 fn main() {
-    for i in 1..12 {
-        let rand_num = fastrand::i32(-23..43);
-        println!("{}", rand_num);
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 3 { /*if there are enough args. len includes zero */
+        let amount: i16 = args[1].parse().unwrap(); /* get shift input from args*/
+        let length: i16 = args[2].parse().unwrap(); /* get shift input from args*/
+
+        for _i in 0..amount {
+            let rand_str: String = std::iter::repeat_with(fastrand::alphanumeric)
+                .take(length.try_into().unwrap())   /* length of string*/
+                .collect();
+            println!("{}", rand_str);
+        }
+
+    } else {
+        println!("Usage: [AMOUNT] [LENGTH]");
+        println!(" This is a Program That Prints Out Random Character");
+        println!(" Passwords for Your Use.");
     }
 }
