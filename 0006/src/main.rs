11de7584b489fcef4cf39406880e6103b436c76b
Your Name <originating_cagliostro@aleeas.com>
2022-11-04 15:18:10 -0500

    Updateing

--- 0006/src/main.rs
+++ 0006/src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+
+const PROGRAMENAME: &str = "numberline";
+
+fn print_line() {
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 2 {
+        if args[1] == "--help" {
+            println!("Usage: {} [flags]", PROGRAMENAME);
+            println!("  --help      display this help and exit");
+            println!("  -n          no output to file");
+            return;
+        }
+    }
+    println!("this is after");
 }
