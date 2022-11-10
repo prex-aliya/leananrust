7651e3c03abff1b0de5497d80b3b22b01815e6a3
Your Name <originating_cagliostro@aleeas.com>
2022-11-10 14:13:08 -0600

    backup

--- Line_Renderer/src/main.rs
+++ Line_Renderer/src/main.rs
@@ -6,15 +6,18 @@ const SSIZE: i16 = 16;
 fn print_help() {
     println!("Usage: {} [--help/num] [num] [num] [num]", PROGRAMENAME);
 }
+
 fn calc(points: [i16; 4]) -> [f32; 2] {
+    /* points to float for a more complete */
+    let fx: f32 = points[0].into();
+    let fy: f32 = points[1].into();
+    let fx1: f32 = points[2].into();
+    let fy1: f32 = points[3].into();
+
     /* y = mx+b
      * find, m, slope
      */
-    let m: f32 = ((points[3]-points[1])/(points[2]-points[0])).into();
-
-    /* make x and y a float */
-    let fx: f32 = points[0].into();
-    let fy: f32 = points[1].into();
+    let m: f32 = ((fy1-fy)/(fx1-fx)).into();
 
     /* find y intercept or b */
     let b: f32 = ((m*fx)-fy).into();
@@ -22,29 +25,41 @@ fn calc(points: [i16; 4]) -> [f32; 2] {
     let output: [f32; 2] = [m, b];
     return output;
 }
+fn calc_line(x: f32, m: f32, b: f32) -> f32 {
+    let line: f32 = (m*x)+b;
+
+    return line;
+}
 fn print_screen(points: [i16; 4]) {
+    /* points to float */
+    let fx: f32 = points[0].into();
+    let fy: f32 = points[1].into();
+    let fx1: f32 = points[2].into();
+    let fy1: f32 = points[3].into();
 
     /* Print Inputs and Calculated */
     let calc: [f32; 2] = calc(points);
+    let m: f32 = calc[0];
+    let b: f32 = calc[1];
     println!("Slope, y-intercept: {:?}", calc);
-
 
     for y in (0..SSIZE).rev() {
         for x in 0..SSIZE {
-            let fy: f32 = y.into(); /* convert i16 to f32 for capability */
-            //let fx: f32 = y.into();
-            let line: i16 = (((fy-calc[1])/calc[0])).round() as i16;
-            //let liney: i16 = (((calc[0]*fx)+calc[1])).round() as i16;
+
+            let fx: f32 = x.into();
+            let fy: f32 = y.into();
+            let line: f32 = calc_line(fx, m, b);
 
             if x == points[0] && y == points[1]
                 || x == points[2] && y == points[3]
             {
+                //print!("..");
                 print!("..");
-            } else if x == line-2 {
+            } else if fy == line {
                 print!(":]");
             } else {
-                print!("#@");
-                //print!("{}", line);
+               // print!("#@");
+                print!("{}", line.round());
             }
         }
         print!("\n");
