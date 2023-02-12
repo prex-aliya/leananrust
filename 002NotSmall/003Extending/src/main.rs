c4a9b7063f4e1fb214da43370238b52a80db7b05
prex-aliya <droved-through@proton.me>
2023-02-12 12:57:29 -1000

    update on extensable 003Extending

--- 002NotSmall/003Extending/src/main.rs
+++ 002NotSmall/003Extending/src/main.rs
@@ -1,31 +1,55 @@
+use ncurses::*;
 mod extensions {
+    use ncurses::*;
     pub fn begin() {
         /* calles external list */
-        println!("begin!");
+        //addstr("begin!\n");
     }
 
+
     pub fn move_left() {
-        println!("moved left!");
+        addstr("moved left!");
     }
+    pub fn move_right() {
+        addstr("moved right!");
+    }
+    pub fn move_up() {
+        addstr("moved up!");
+    }
+    pub fn move_down() {
+        addstr("moved down!");
+    }
+
 
     pub fn end() {
         /* calles external list */
-        println!("end!\n");
+        //addstr("\nend!\n");
     }
 }
 
 
 
 fn main() {
+    initscr();
+    noecho(); /* Stops showing of input keys */
+
     println!("Hello, world!");
 
-    loop {
-
-
+    'main :loop {
         extensions::begin();
 
-        extensions::move_left();
+        let key = getch();
+        match key as u8 as char {
+            'h' => extensions::move_left(),
+            'l' => extensions::move_right(),
+            'k' => extensions::move_up(),
+            'j' => extensions::move_down(),
+            'q' => { break 'main; },
+            _ => {},
+        }
 
         extensions::end();
     }
+
+    endwin(); /* Restores terminal behavior */
 }
